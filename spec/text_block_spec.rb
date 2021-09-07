fe79b77e8b19a622ab95551ee59956572d97a7bd
Sergey Konotopov <lalalalalala@gmail.com>
2021-09-07 20:51:41 +0300

    Add a spec for InvadersFinder::TextBlock#to_a

--- spec/text_block_spec.rb
+++ spec/text_block_spec.rb
@@ -9,5 +9,19 @@ RSpec.describe InvadersFinder::TextBlock do
         expect(text_block.to_a).to eq(%w[-o])
       end
     end
+
+    let(:simple_test_data) do
+      <<~STR
+       ab
+       cd
+      STR
+    end
+
+    let(:simple_text_block) { InvadersFinder::TextBlock.new(simple_test_data) }
+
+    specify '#to_a returns an array of strings representing the text block' do
+      expect(simple_text_block.to_a).to be_instance_of(Array)
+      expect(simple_text_block.to_a[0]).to be_instance_of(String)
+    end
   end
 end
