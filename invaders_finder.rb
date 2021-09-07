5e7a7cd6b2c24c0564a695858aea707b266a565e
Sergey Konotopov <lalalalalala@gmail.com>
2021-09-08 00:09:59 +0300

    Add InvadersFinder::Main

--- invaders_finder.rb
+++ invaders_finder.rb
@@ -16,4 +16,35 @@ module InvadersFinder
 
   PAD_CHAR = 'x' # Used to pad the sample data, to detect patterns at edges
   NEWLINE = "\n" # System-specific line separator
+
+  SIMILARITY_ALGORITHMS = { # Used to detect similarity between a sample and a pattern
+    levenshtein: SimilarityAlgorithms::Levenshtein,
+    white_similarity: SimilarityAlgorithms::WhiteSimilarity
+  }.freeze
+  USE_ALGORITHM = :levenshtein
+
+  SAMPLE_DATA_FILE = 'data/sample_data.txt' # Radar sample data
+  PATTERNS_DATA_FILE = 'data/patterns.yml' # Space invader patterns
+
+  LOGGER = ScreenLogger.new # Where to output
+
+  # Entry point class
+  class Main
+    def self.execute # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+      sample = TextBlock.new(File.read(SAMPLE_DATA_FILE), :radar_sample)
+      pattern_data = YAML.load_file(PATTERNS_DATA_FILE)
+      invaders_names = pattern_data.keys
+
+      invaders_names.each do |name|
+        pattern = TextBlock.new(pattern_data[name], name.to_sym)
+        LOGGER.log("Finding #{pattern.name.capitalize}...", :bright)
+
+        similarity_algorithm = SIMILARITY_ALGORITHMS[USE_ALGORITHM].new
+        matches = Scanner.new(sample, pattern, similarity_algorithm).execute
+
+        LOGGER.log("Found #{matches.size} matches.", matches.size.zero? ? :red : :green)
+        matches.each(&:log)
+      end
+    end
+  end
 end
---
+++ run.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require_relative 'invaders_finder.rb'
+
+InvadersFinder::Main.execute
